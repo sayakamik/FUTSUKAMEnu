be68ccd3f3c1c59eaf396745f590209d110728a3
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-10-06 08:35:11 +0000

    public controller view作成

---
+++ app/controllers/public/recipes_controller.rb
@@ -0,0 +1,16 @@
+class Public::RecipesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def draft_index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'post_comments/index'
+  end
+  namespace :public do
+    get 'favorites/index'
+  end
+  namespace :public do
+    get 'recipes/new'
+    get 'recipes/index'
+    get 'recipes/show'
+    get 'recipes/edit'
+    get 'recipes/draft_index'
+  end
+  namespace :public do
+    get 'users/mypage'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/confirm_withdraw'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :users, skip: [:passwords] , controllers: {
