9d147f85a54d1c64795c44889e42865f6b536c0c
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-10-07 11:00:51 +0000

    add top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @recipes = Recipe.where(is_draft: false).limit(4).order("created_at DESC")
+    @original_menus = OriginalMenu.all
   end
 
   def about
--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -3,6 +3,19 @@ class Public::RecipesController < ApplicationController
   end
 
   def index
+    #1日目メニュー一覧表示
+    @original_menus= OriginalMenu.all
+    #レシピ一覧表示
+    #クエリパラメータ(original_menu_id)をとりだす
+    if @original_menu_id = params[:original_menu_id]
+      #original_menu_idが同じものを全てとりだす
+      @recipes = Recipe.where(original_menu_id: @original_menu_id).page(params[:page]).per(10)
+    #なければ全てとりだす
+    elsif recipe_name = params[:recipe_name]
+      @recipes = Recipe.where("name LIKE ?","%"+ recipe_name + "%").page(params[:page]).per(10)
+    else
+      @recipes = Recipe.all.page(params[:page]).per(10)
+    end
   end
 
   def show
