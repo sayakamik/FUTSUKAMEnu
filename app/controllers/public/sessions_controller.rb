a9d8c4f58ff9c8edfb7e9060111a78b38ffa183c
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-10-07 09:43:39 +0000

    add About,log-in,sign-up,sign-in,log-out

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,6 +24,14 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   def guest_sign_in
     user = User.guest
     sign_in user
