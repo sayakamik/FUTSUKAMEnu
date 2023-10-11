70713e8d08d1dd1eaf4673ec4ece2312078dd18e
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-10-11 01:25:40 +0000

    favorite-index

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,5 +1,18 @@
 class Public::FavoritesController < ApplicationController
   def index
+    #1日目メニュー一覧表示
+    @original_menus = OriginalMenu.joins(:recipes)
+       .where(recipes: { is_draft: false }) # 公開されたレシピのみを選択
+       .order("recipes.created_at DESC")    # レシピの作成日時で降順にソート
+       .limit(10)                          # 上位10件を取得
+       .uniq                              #重複削除
+    # 下書きでないレシピ一覧表示
+    @user = current_user
+    @favorites = Favorite.where(user_id: @user.id).all
+    @recipe_ids = @favorites.pluck(:recipe_id) # お気に入りレシピのIDを取得
+    # レシピを取得
+    @recipes = Recipe.where(id: @recipe_ids, is_draft: false).page(params[:page]).per(10)
+    @recipes_count = @recipes.all
   end
 
   def create
--- app/controllers/public/original_menus_controller.rb
+++ app/controllers/public/original_menus_controller.rb
@@ -2,6 +2,7 @@ class Public::OriginalMenusController < ApplicationController
   def index
     @original_menus = OriginalMenu.joins(:recipes)
       .where(recipes: { is_draft: false })
+      .select('DISTINCT original_menus.*') #重複を避ける.uniqはここでは使えなかった。
       .order("original_menus.name ASC") # ここで name カラムを使って 五十音順（昇順）にソート
       .page(params[:page]).per(50)
 
