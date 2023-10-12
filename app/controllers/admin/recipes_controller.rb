090103965668c08cfa3e6e4f606653291b485652
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-10-12 10:57:21 +0000

    add admin recipe-index

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,8 +1,11 @@
 class Admin::RecipesController < ApplicationController
   def index
+    @recipes = Recipe.where(is_draft: false).page(params[:page]).per(15)
+    @recipes_count = @recipes.all
   end
 
   def show
+    
   end
 
   def edit
