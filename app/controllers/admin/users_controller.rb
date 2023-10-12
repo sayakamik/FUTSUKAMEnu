8c566daa7e4df5c1eaab611ba93c82c5e84675f0
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-10-12 06:36:23 +0000

    add admin-user-show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,28 @@
 class Admin::UsersController < ApplicationController
   def show
-     @customer= Customer.find(params[:id])
+    @user = User.find(params[:id])
+    @recipes = @user.recipes.where(is_draft: false).page(params[:page]).per(10)
+    @recipes_count = @recipes.all
   end
 
   def edit
+    @user= User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       flash[:notice] = "会員情報を変更しました。"
+       redirect_to admin_user_path(@user.id)
+    else
+       render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :is_deleted, :encrypted_password, :profile_image)
+  end
+
 end
