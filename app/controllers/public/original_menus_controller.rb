b4b669de531086cd6a82088be188b501e2805d03
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-10-17 07:38:18 +0000

    add layout-users/favorites/original_menus

--- app/controllers/public/original_menus_controller.rb
+++ app/controllers/public/original_menus_controller.rb
@@ -4,9 +4,8 @@ class Public::OriginalMenusController < ApplicationController
   def index
     @original_menus = OriginalMenu.joins(:recipes)
       .where(recipes: { is_draft: false })
-      .select('DISTINCT original_menus.*') #重複を避ける.uniqはここでは使えなかった。
       .order("original_menus.name ASC") # ここで name カラムを使って 五十音順（昇順）にソート
+      .select('DISTINCT original_menus.*') #重複を避ける.uniqはここでは使えなかった。
       .page(params[:page]).per(50)
-
   end
 end
